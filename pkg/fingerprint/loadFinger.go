17059ab673c716257f82088610c0c0a289b8a27e
chobits02 <60724505+youki992@users.noreply.github.com>
2024-08-13 09:07:01 +0800

    Update loadFinger.go

--- pkg/fingerprint/loadFinger.go
+++ pkg/fingerprint/loadFinger.go
@@ -8,6 +8,10 @@ type Packjson struct {
 	Fingerprint []Fingerprint
 }
 
+type PackjsonSubdomain struct {
+	Fingerprint []FingerprintRecord
+}
+
 type Fingerprint struct {
 	Cms      string
 	Method   string
@@ -15,8 +19,17 @@ type Fingerprint struct {
 	Keyword  []string
 }
 
+type FingerprintRecord struct {
+	Cname       []string
+	Discussion  string
+	Fingerprint string
+	Service     string
+	Status      string
+}
+
 var (
-	Webfingerprint *Packjson
+	Webfingerprint       *Packjson
+	Subdomainfingerprint *PackjsonSubdomain
 )
 
 func LoadWebfingerprintEhole() error {
@@ -39,10 +52,24 @@ func LoadWebfingerprintLocal() error {
 	return nil
 }
 
+func LoadSubdomainfingerprint() error {
+	var config PackjsonSubdomain
+	err := json.Unmarshal([]byte(subdomainFinger), &config)
+	if err != nil {
+		return err
+	}
+	Subdomainfingerprint = &config
+	return nil
+}
+
 func GetWebfingerprintLocal() *Packjson {
 	return Webfingerprint
 }
 
 func GetWebfingerprintEhole() *Packjson {
 	return Webfingerprint
+}
+
+func GetWebfingerprintSubdomain() *PackjsonSubdomain {
+	return Subdomainfingerprint
 }
