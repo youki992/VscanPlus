a769c4a4dfa8b4201a39692d3e8931587b63a32f
chobits02 <60724505+youki992@users.noreply.github.com>
2024-08-13 09:06:32 +0800

    Update fingerScan.go

--- pkg/fingerprint/fingerScan.go
+++ pkg/fingerprint/fingerScan.go
@@ -2,10 +2,16 @@ package fingerprint
 
 import (
 	"encoding/json"
+	"fmt"
+	"net"
+	"os"
+	"regexp"
+	"strings"
 )
 
 var EholeFinpx *Packjson
 var LocalFinpx *Packjson
+var SubdomainFinpx *PackjsonSubdomain
 
 func New() error {
 	err := LoadWebfingerprintEhole()
@@ -20,12 +26,53 @@ func New() error {
 	}
 	LocalFinpx = GetWebfingerprintLocal()
 	return nil
+
+	err = LoadSubdomainfingerprint()
+	if err != nil {
+		return err
+	}
+	SubdomainFinpx = GetWebfingerprintSubdomain()
+	return nil
 }
 
 func mapToJson(param map[string][]string) string {
 	dataType, _ := json.Marshal(param)
 	dataString := string(dataType)
 	return dataString
+}
+
+// 提取URL中的域名
+func extractDomain(url string) string {
+	re := regexp.MustCompile(`(?i)^(?:https?://)?([^/]+)`)
+	matches := re.FindStringSubmatch(url)
+	if len(matches) > 1 {
+		return matches[1]
+	}
+	return ""
+}
+
+// 记录匹配的域名到文件
+func logMatchedDomain(domain string) {
+	file, err := os.OpenFile("matched_domains.txt", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		fmt.Println("Error opening file:", err)
+		return
+	}
+	defer file.Close()
+
+	if _, err := file.WriteString(domain + "\n"); err != nil {
+		fmt.Println("Error writing to file:", err)
+	}
+}
+
+// 执行DNS查询并返回CNAME记录值
+func dnsCNAME(domain string) (string, error) {
+	resolver := new(net.Resolver)
+	answers, err := resolver.LookupCNAME(nil, domain)
+	if err != nil {
+		return "", err
+	}
+	return answers, nil
 }
 
 func FingerScan(headers map[string][]string, body []byte, title string, url string) []string {
@@ -38,6 +85,20 @@ func FingerScan(headers map[string][]string, body []byte, title string, url stri
 			if finp.Method == "keyword" {
 				if iskeyword(bodyString, finp.Keyword) {
 					cms = append(cms, finp.Cms)
+				}
+				// 记录匹配的域名
+				domain := extractDomain(url)
+				recordValue, err := dnsCNAME(domain)
+				if err == nil {
+					for _, dinp := range SubdomainFinpx.Fingerprint {
+						for _, cname := range dinp.Cname {
+							fmt.Println(dinp.Fingerprint)
+							if strings.Contains(recordValue, cname) && strings.Contains(bodyString, dinp.Fingerprint) {
+								// 如果DNS记录值中包含Cname中的关键词，记录域名
+								logMatchedDomain(domain + " / " + dinp.Service + " " + dinp.Discussion)
+							}
+						}
+					}
 				}
 			}
 			if finp.Method == "faviconhash" {
